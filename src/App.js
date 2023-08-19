0f47de8d04bc4a518d8880ca38cf09b6a70900e8
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-19 16:29:59 +0900

    6th update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import About from './pages/About';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
+import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 
 const App = () => {
@@ -17,6 +20,9 @@ const App = () => {
             <Route path="/articles" element={<Articles />}>
                 <Route path=":id" element={<Article />} />
             </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path=".mypage" element={<MyPage />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
